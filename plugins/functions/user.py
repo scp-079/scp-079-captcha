4d5a92e64b9a990ca0c134836aea1eb1ddcd6974
Xiao <xiao@scp-079.org>
2019-10-21 13:17:12 +0000

    - 0.0.1

---
+++ plugins/functions/user.py
@@ -0,0 +1,88 @@
+# SCP-079-CAPTCHA - Provide challenges for new joined members
+# Copyright (C) 2019 SCP-079 <https://scp-079.org>
+#
+# This file is part of SCP-079-CLEAN.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+from time import sleep
+from typing import Union
+
+from pyrogram import ChatPermissions, Client
+
+from .. import glovar
+from .etc import thread
+from .ids import init_user_id
+from .telegram import kick_chat_member, restrict_chat_member, unban_chat_member
+
+# Enable logging
+logger = logging.getLogger(__name__)
+
+
+def add_detected_user(gid: int, uid: int, now: int) -> bool:
+    # Add or update a detected user's status
+    try:
+        if not init_user_id(uid):
+            return False
+
+        previous = glovar.user_ids[uid]["failed"].get(gid)
+        glovar.user_ids[uid]["failed"][gid] = now
+
+        return bool(previous)
+    except Exception as e:
+        logger.warning(f"Add detected user error: {e}", exc_info=True)
+
+    return False
+
+
+def ban_user(client: Client, gid: int, uid: Union[int, str]) -> bool:
+    # Ban a user
+    try:
+        if glovar.configs[gid].get("restrict"):
+            thread(restrict_chat_member, (client, gid, uid, ChatPermissions()))
+        else:
+            thread(kick_chat_member, (client, gid, uid))
+
+        return True
+    except Exception as e:
+        logger.warning(f"Ban user error: {e}", exc_info=True)
+
+    return False
+
+
+def kick_user(client: Client, gid: int, uid: Union[int, str]) -> bool:
+    # Kick a user
+    try:
+        kick_chat_member(client, gid, uid)
+        sleep(3)
+        unban_chat_member(client, gid, uid)
+
+        return True
+    except Exception as e:
+        logger.warning(f"Kick user error: {e}", exc_info=True)
+
+    return False
+
+
+def unban_user(client: Client, gid: int, uid: int) -> bool:
+    # Unban a user
+    try:
+        thread(unban_chat_member, (client, gid, uid))
+
+        return True
+    except Exception as e:
+        logger.warning(f"Unban user error: {e}", exc_info=True)
+
+    return False
---
+++ plugins/handlers/command.py
@@ -0,0 +1,215 @@
+# SCP-079-CAPTCHA - Provide challenges for new joined members
+# Copyright (C) 2019 SCP-079 <https://scp-079.org>
+#
+# This file is part of SCP-079-CLEAN.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import re
+from copy import deepcopy
+
+from pyrogram import Client, Filters, Message
+
+from .. import glovar
+from ..functions.channel import get_debug_text, share_data
+from ..functions.etc import bold, code, delay, get_command_context, get_command_type, get_now, lang
+from ..functions.etc import thread, user_mention
+from ..functions.file import save
+from ..functions.filters import from_user, is_class_c, test_group
+from ..functions.group import delete_message, get_config_text
+from ..functions.telegram import get_group_info, send_message, send_report_message
+
+# Enable logging
+logger = logging.getLogger(__name__)
+
+
+@Client.on_message(Filters.incoming & Filters.group & ~test_group & from_user
+                   & Filters.command(["config"], glovar.prefix))
+def config(client: Client, message: Message) -> bool:
+    # Request CONFIG session
+
+    if not message or not message.chat:
+        return True
+
+    # Basic data
+    gid = message.chat.id
+    mid = message.message_id
+
+    try:
+        # Check permission
+        if not is_class_c(None, message):
+            return True
+
+        # Check command format
+        command_type = get_command_type(message)
+        if not command_type or not re.search(f"^{glovar.sender}$", command_type, re.I):
+            return True
+
+        now = get_now()
+
+        # Check the config lock
+        if now - glovar.configs[gid]["lock"] < 310:
+            return True
+
+        # Set lock
+        glovar.configs[gid]["lock"] = now
+        save("configs")
+
+        # Ask CONFIG generate a config session
+        group_name, group_link = get_group_info(client, message.chat)
+        share_data(
+            client=client,
+            receivers=["CONFIG"],
+            action="config",
+            action_type="ask",
+            data={
+                "project_name": glovar.project_name,
+                "project_link": glovar.project_link,
+                "group_id": gid,
+                "group_name": group_name,
+                "group_link": group_link,
+                "user_id": message.from_user.id,
+                "config": glovar.configs[gid],
+                "default": glovar.default_config
+            }
+        )
+
+        # Send debug message
+        text = get_debug_text(client, message.chat)
+        text += (f"{lang('admin_group')}{lang('colon')}{code(message.from_user.id)}\n"
+                 f"{lang('action')}{lang('colon')}{code(lang('config_create'))}\n")
+        thread(send_message, (client, glovar.debug_channel_id, text))
+
+        return True
+    except Exception as e:
+        logger.warning(f"Config error: {e}", exc_info=True)
+    finally:
+        if is_class_c(None, message):
+            delay(3, delete_message, [client, gid, mid])
+        else:
+            delete_message(client, gid, mid)
+
+    return False
+
+
+@Client.on_message(Filters.incoming & Filters.group & ~test_group & from_user
+                   & Filters.command([f"config_{glovar.sender.lower()}"], glovar.prefix))
+def config_directly(client: Client, message: Message) -> bool:
+    # Config the bot directly
+
+    if not message or not message.chat:
+        return True
+
+    # Basic data
+    gid = message.chat.id
+    mid = message.message_id
+
+    try:
+        # Check permission
+        if not is_class_c(None, message):
+            return True
+
+        aid = message.from_user.id
+        success = True
+        reason = lang("config_updated")
+        new_config = deepcopy(glovar.configs[gid])
+        text = f"{lang('admin_group')}{lang('colon')}{code(aid)}\n"
+        # Check command format
+        command_type, command_context = get_command_context(message)
+        if command_type:
+            if command_type == "show":
+                text += f"{lang('action')}{lang('colon')}{code(lang('config_show'))}\n"
+                text += get_config_text(new_config)
+                thread(send_report_message, (30, client, gid, text))
+                return True
+
+            now = get_now()
+            # Check the config lock
+            if now - new_config["lock"] > 310:
+                if command_type == "default":
+                    new_config = deepcopy(glovar.default_config)
+                else:
+                    if command_context:
+                        if command_type in {"delete", "restrict", "ban", "forgive", "hint"}:
+                            if command_context == "off":
+                                new_config[command_type] = False
+                            elif command_context == "on":
+                                new_config[command_type] = True
+                            else:
+                                success = False
+                                reason = lang("command_para")
+
+                            if command_type == "restrict" and new_config[command_type]:
+                                new_config["ban"] = False
+                            elif command_type == "ban" and new_config[command_type]:
+                                new_config["restrict"] = False
+                        else:
+                            success = False
+                            reason = lang("command_type")
+                    else:
+                        success = False
+                        reason = lang("command_lack")
+
+                    if success:
+                        new_config["default"] = False
+            else:
+                success = False
+                reason = lang("config_locked")
+        else:
+            success = False
+            reason = lang("command_usage")
+
+        if success and new_config != glovar.configs[gid]:
+            # Save new config
+            glovar.configs[gid] = new_config
+            save("configs")
+
+            # Send debug message
+            debug_text = get_debug_text(client, message.chat)
+            debug_text += (f"{lang('admin_group')}{lang('colon')}{code(message.from_user.id)}\n"
+                           f"{lang('action')}{lang('colon')}{code(lang('config_change'))}\n"
+                           f"{lang('more')}{lang('colon')}{code(f'{command_type} {command_context}')}\n")
+            thread(send_message, (client, glovar.debug_channel_id, debug_text))
+
+        text += (f"{lang('action')}{lang('colon')}{code(lang('config_change'))}\n"
+                 f"{lang('status')}{lang('colon')}{code(reason)}\n")
+        thread(send_report_message, ((lambda x: 10 if x else 5)(success), client, gid, text))
+
+        return True
+    except Exception as e:
+        logger.warning(f"Config directly error: {e}", exc_info=True)
+    finally:
+        delete_message(client, gid, mid)
+
+    return False
+
+
+@Client.on_message(Filters.incoming & Filters.group & test_group & from_user
+                   & Filters.command(["version"], glovar.prefix))
+def version(client: Client, message: Message) -> bool:
+    # Check the program's version
+    try:
+        cid = message.chat.id
+        aid = message.from_user.id
+        mid = message.message_id
+        text = (f"{lang('admin')}{lang('colon')}{user_mention(aid)}\n\n"
+                f"{lang('version')}{lang('colon')}{bold(glovar.version)}\n")
+        thread(send_message, (client, cid, text, mid))
+
+        return True
+    except Exception as e:
+        logger.warning(f"Version error: {e}", exc_info=True)
+
+    return False
