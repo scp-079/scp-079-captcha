14431a18bd7bcc779335056129ace70bd5dbcd12
Xiao <xiao@scp-079.org>
2019-10-22 23:28:26 +0000

    - 0.0.2

--- main.py
+++ main.py
@@ -43,11 +43,11 @@ update_status(app, "online")
 
 # Timer
 scheduler = BackgroundScheduler(job_defaults={"misfire_grace_time": 60})
-scheduler.add_job(new_invite_link, "interval", [app], seconds=glovar.time_invite)
+scheduler.add_job(new_invite_link, "interval", [app, "edit"], seconds=glovar.time_invite)
 scheduler.add_job(interval_min_01, "interval", [app], minutes=1)
 scheduler.add_job(interval_min_10, "interval", minutes=10)
 scheduler.add_job(update_status, "cron", [app, "awake"], minute=30)
-scheduler.add_job(new_invite_link, "cron", [app], hour=1)
+scheduler.add_job(new_invite_link, "cron", [app, "new"], hour=1)
 scheduler.add_job(backup_files, "cron", [app], hour=20)
 scheduler.add_job(send_count, "cron", [app], hour=21)
 scheduler.add_job(reset_data, "cron", [app], day=glovar.date_reset, hour=22)
