4281f1fa680b3eb877c94bc6f3e07342d7fa3218
Xiao <xiao@scp-079.org>
2020-02-20 14:16:54 +0000

    - 0.4.0

--- plugins/functions/timers.py
+++ plugins/functions/timers.py
@@ -378,6 +378,13 @@ def update_admins(client: Client) -> bool:
                                 and admin.can_restrict_members):
                             should_leave = False
 
+                        # TODO
+                        # if (admin.user.is_self
+                        #         and admin.can_delete_messages
+                        #         and admin.can_restrict_members
+                        #         and admin.can_pin_messages):
+                        #     should_leave = False
+
                 if not should_leave:
                     continue
 
