1489e63820233223d3c9d41e6a3dca4c81949ca9
Xiao <xiao@scp-079.org>
2020-05-31 14:55:27 +0000

    略做修改

--- plugins/functions/filters.py
+++ plugins/functions/filters.py
@@ -527,8 +527,7 @@ def is_flooded(gid: int) -> bool:
     result = False
 
     try:
-        init_group_id(gid)
-        result = glovar.pinned_ids[gid]["start"]
+        result = glovar.pinned_ids.get(gid) and glovar.pinned_ids[gid]["start"]
     except Exception as e:
         logger.warning(f"Is flooded error: {e}", exc_info=True)
 
