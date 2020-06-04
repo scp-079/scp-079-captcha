251b31010bc6696915f6b9366bdbd8b4d09174b3
Xiao <xiao@scp-079.org>
2020-06-04 05:15:18 +0000

    0.6.0

--- plugins/functions/captcha.py
+++ plugins/functions/captcha.py
@@ -1121,7 +1121,7 @@ def send_hint_qns(client: Client, the_type: str, gid: int,
         if the_type in {"manual", "single"}:
             tags = list(glovar.questions[gid]["qns"])
         else:
-            tags = glovar.questions[gid]["last"]
+            tags = [glovar.questions[gid]["last"]] or list(glovar.questions[gid]["qns"])
 
         if not tags:
             return False
