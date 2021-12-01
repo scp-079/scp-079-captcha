fc4acf2d96843e4b541d11af5297cf5695b67870
Xiao <xiao@scp-079.org>
2021-12-01 23:14:33 +0200

    update layer

--- plugins/handlers/message.py
+++ plugins/handlers/message.py
@@ -79,8 +79,8 @@ def hint(client: Client, message: Message) -> bool:
 
             # Check user status
             test_result = (glovar.user_ids.get(uid, {})
-                    and (glovar.user_ids[uid]["wait"].get(gid, 0)
-                         or (glovar.user_ids[uid]["failed"].get(gid, 0) > 0 and get_level(gid) != "kick")))
+                           and (glovar.user_ids[uid]["wait"].get(gid, 0)
+                                or (glovar.user_ids[uid]["failed"].get(gid, 0) > 0 and get_level(gid) != "kick")))
             if test_result:
                 logger.warning(f"result {test_result}")
                 logger.warning(glovar.user_ids[uid]["wait"].get(gid, 0))
